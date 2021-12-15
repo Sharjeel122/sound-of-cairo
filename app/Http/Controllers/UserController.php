9149c58a4e5ace76e663d1d1ce4de00e3de5458f
Abdul Basit <abdulbasit167326@gmail.com>
2021-12-15 12:18:17 -0800

    user block , unblock , Get by id and get all users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,8 +25,8 @@ class UserController extends Controller
             return response()->json($errors ,400);
         }
         elseif($request->agree_on_terms  == false){
-            $errors =['Message'=>['Must agree with terms of services']];
-            return response()->json($errors ,400);
+            $result =['Message'=>['Must agree with terms of services']];
+            return response()->json($result ,400);
         }
         else
         {
@@ -48,8 +48,8 @@ class UserController extends Controller
         $user = User::where('email', $request->email)->first();
         if (!$user || !Hash::check($request->password, $user->password))
         {
-            $errors =['Message'=>['Bad username or password']];
-            return response()->json($errors ,400);
+            $result =['Message'=>['Bad username or password']];
+            return response()->json($result ,400);
         }
         else
         {
@@ -63,19 +63,67 @@ class UserController extends Controller
 
     public function Logout(Request $request)
     {
-        if($request->user('sanctum'))
-        {
+
             $id = $request->user('sanctum')->id;
             $user = User::findOrFail($id);
             $user->tokens()->delete();
-            $success =['Message'=>['Logout Successfully']];
-            return response()->json($success  ,200);
+            $result =['Message'=>['Logout Successfully']];
+            return response()->json($result  ,200);
+
+    }
+
+    public function GetAll()
+    {
+        $users = User::where('status',1)->get();
+        return response()->json(['users'=>$users]);
+    }
+
+    public function Get($id)
+    {
+        $user = User::where('id',$id)->first();
+        if($user != null)
+        {
+            return response()->json(['user'=>$user]);
         }
         else
         {
-            $errors =['Message'=>['UnAuthenticated']];
-            return response()->json($errors ,401);
+            $result =['Message'=>['User Not Found']];
+            return response()->json($result ,404);
         }
+
+    }
+
+    public function Block($id)
+    {
+        $user =  User::where('id',$id)->first();
+        if($user != null)
+        {
+            $user->status = false ;
+            $user->update();
+            return response()->json(['user'=>$user]);
+        }
+        else
+        {
+            $result =['Message'=>['User Not Found']];
+            return response()->json($result ,404);
+        }
+
+    }
+    public function UnBlock($id)
+    {
+        $user = User::where('id',$id)->first();
+        if($user != null)
+        {
+            $user->status = true ;
+            $user->update();
+            return response()->json(['user'=>$user]);
+        }
+        else
+        {
+            $result =['Message'=>['User Not Found']];
+            return response()->json($result ,404);
+        }
+
     }
 
 }
