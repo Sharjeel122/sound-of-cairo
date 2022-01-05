a2df477014432b77b6a1911fd721aa5699700e49
Abdul Basit <abdulbasit167326@gmail.com>
2022-01-05 14:42:17 -0800

    Location resource is added in this branch

--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,9 @@ Route::get('/tag/all/{pageNum}', 'App\Http\Controllers\TagController@GetAll');
 //Get Category
 Route::get('/category/{id}', 'App\Http\Controllers\CategoryController@Get');
 Route::get('/category/all/{pageNum}', 'App\Http\Controllers\CategoryController@GetAll');
+//Get Location
+Route::get('/location/{id}', 'App\Http\Controllers\LocationController@Get');
+Route::get('/location/all/{pageNum}', 'App\Http\Controllers\LocationController@GetAll');
 
  Route::group(['middleware' =>  'auth:sanctum','json.response' ], function () {
     Route::post('/user/logout', 'App\Http\Controllers\UserController@Logout');
@@ -39,15 +42,21 @@ Route::get('/category/all/{pageNum}', 'App\Http\Controllers\CategoryController@G
     Route::put('/user-profile/user/delete/{id}', 'App\Http\Controllers\UserProfileController@delete');
     Route::put('/user-profile/user/update/{id}', 'App\Http\Controllers\UserProfileController@update');
 
-//    Category Routes
+     // Category Routes
      Route::post('/category', 'App\Http\Controllers\CategoryController@Add');
      Route::put('/category/{id}', 'App\Http\Controllers\CategoryController@Update');
      Route::put('/category/block/{id}', 'App\Http\Controllers\CategoryController@Block');
      Route::put('/category/unblock/{id}', 'App\Http\Controllers\CategoryController@Unblock');
 
-     //    Tag Routes
+     // Tag Routes
      Route::post('/tag', 'App\Http\Controllers\TagController@Add');
      Route::put('/tag/{id}', 'App\Http\Controllers\TagController@Update');
      Route::put('/tag/block/{id}', 'App\Http\Controllers\TagController@Block');
      Route::put('/tag/unblock/{id}', 'App\Http\Controllers\TagController@Unblock');
+
+     // Location Routes
+     Route::post('/location', 'App\Http\Controllers\LocationController@Add');
+     Route::put('/location/{id}', 'App\Http\Controllers\LocationController@Update');
+     Route::put('/location/block/{id}', 'App\Http\Controllers\LocationController@Block');
+     Route::put('/location/unblock/{id}', 'App\Http\Controllers\LocationController@Unblock');
  });
