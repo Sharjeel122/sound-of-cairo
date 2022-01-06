cd3d3b54aafad1c742257c8fed64368cbb446c07
Abdul Basit <abdulbasit167326@gmail.com>
2022-01-06 15:22:56 -0800

    api routes setup

--- app/Http/Controllers/API/LocationController.php
+++ app/Http/Controllers/API/LocationController.php
@@ -1,9 +1,10 @@
 <?php
-
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
+use App\Http\Controllers\Controller;
 use App\Models\Location;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+
 
 class LocationController extends Controller
 {
